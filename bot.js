bd23117cfc2d6f7b0d39876183f3c85a5788dbcb
Sam McGahan <smcgahan@newrelic.com>
2017-11-15 17:59:03 -0800

    updating use of token

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
-require ('newrelic');
+var auth = process.env.TOKEN;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -10,7 +9,7 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: auth,
    autorun: true
 });
 bot.on('ready', function (evt) {
