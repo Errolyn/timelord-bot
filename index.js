7cf412e68ed8730d7bf37e386f07acf05dc4c537
Riddlerat <smcgahan@me.com>
2020-06-20 22:16:07 -0700

    Basic dice roller

--- index.js
+++ index.js
@@ -57,6 +57,18 @@ bot.registerCommand(
     }
 );
 
+bot.registerCommand(
+    "roll",
+    (msg) => {
+        console.log(rollDecider(stripContent(msg.content)));
+        return rollDecider(stripContent(msg.content));
+    },
+    {
+        description: "Rolls Dice and outputs value",
+        fullDescription: "Use this command with typical dice notation `amount d sides + modifier` add an `r` at the end for rerolling 1s e.g. !roll 2d4 or 5d6+4 or 2d20+2r "
+    }
+)
+
 bot.connect();
 
 // helper functions
@@ -76,4 +88,53 @@ function createMessage(channel, content){
 function formatAttachments(attachments = []){
     return attachments.map(attachment => attachment.url)
         .join('\n');
+}
+
+function rollDecider(command){
+
+    const commandCleaned = command.toLowerCase().split(' ').join('').split('r').join(''); // removes spaces and Rs
+    
+    const pulledApart = commandCleaned.split('d');
+    const amount = pulledApart[0];
+    let reroll = false;
+
+    if (command.toLowerCase().includes('r')){
+        reroll = true;
+    }
+
+    
+    let sides, modifier;
+    if (pulledApart[1].includes('+')){
+        [sides, modifier] = pulledApart[1].split('+');
+        return rollDice(amount, sides, reroll) + Number(modifier);
+    } else if (pulledApart[1].includes('-')) {
+        [sides, modifier] = pulledApart[1].split('-');
+        return rollDice(amount, sides, reroll) - Number(modifier);
+    } else {
+        sides = pulledApart[1];
+        return rollDice(amount, sides, reroll);
+    }
+}
+
+function getRandomNumber(max){
+    let randomNumber = Math.floor(Math.random() * max) + 1;
+    return randomNumber;
+}
+
+function rollDice(amount, sides, reroll){
+    let diceTotal = 0;
+    let roll = 0
+    while(roll < amount){
+        let currentRoll = getRandomNumber(sides);
+
+        if(reroll){
+            while(currentRoll <= 1) {
+                currentRoll = getRandomNumber(sides);
+            }
+        }
+
+        diceTotal += currentRoll;
+        roll++;
+    }
+    return diceTotal;
 }
