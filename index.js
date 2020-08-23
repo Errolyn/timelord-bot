0febe22102f48b67062402c82efd9b72e1e65ba1
Michael Cooper <mythmon@gmail.com>
2020-08-23 15:49:18 -0700

    Use command decscriptions from Fluent automatically (#33)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let Eris = require('eris');
 let fetch = require('node-fetch');
 
+let BotWrapper = require('./lib/BotWrapper.js');
 let ftl = require('./lib/ftl.js');
 
 let pingCount = 0;
@@ -14,11 +14,10 @@ require('http')
   })
   .listen(process.env.PORT || 5050);
 
-const bot = new Eris.CommandClient(
+const bot = new BotWrapper(
   process.env.TOKEN,
   {},
   {
-    description: 'A helpful server bot',
     owner: 'Errolyn',
     prefix: '!',
   },
@@ -43,99 +42,71 @@ if (cocChannel) {
   });
 }
 
-bot.registerCommand(
-  'ping',
-  () => {
-    pingCount++;
-    return ftl('ping-response', { pingCount });
-  },
-  {
-    description: ftl('ping-cmd-description'),
-    fullDescription: ftl('ping-cmd-long-description'),
-  },
-);
+bot.registerCommand('ping', () => {
+  pingCount++;
+  return ftl('ping-response', { pingCount });
+});
 
-bot.registerCommand(
-  'news',
-  async (msg) => {
-    const newsChannel = process.env.NEWS_CHANNEL;
-    const contentForNewsChannel = stripContent(msg.content);
+bot.registerCommand('news', async (msg) => {
+  const newsChannel = process.env.NEWS_CHANNEL;
+  const contentForNewsChannel = stripContent(msg.content);
 
-    let userName = 'unknown';
-    let messageChannelName = 'unknown';
-    if (msg.channel.type == 0) {
-      userName = msg.member.nick ? msg.member.nick : msg.author.username;
-      messageChannelName = msg.channel.name;
-    } else if (msg.channel.type == 1) {
-      userName = msg.author.username;
-      messageChannelName = ftl('news-dm-description');
-    }
+  let userName = 'unknown';
+  let messageChannelName = 'unknown';
+  if (msg.channel.type == 0) {
+    userName = msg.member.nick ? msg.member.nick : msg.author.username;
+    messageChannelName = msg.channel.name;
+  } else if (msg.channel.type == 1) {
+    userName = msg.author.username;
+    messageChannelName = ftl('news-dm-description');
+  }
 
-    let content = ftl('news-post-message', {
-      userName,
-      messageChannelName,
-      contentForNewsChannel,
-    });
+  let content = ftl('news-post-message', {
+    userName,
+    messageChannelName,
+    contentForNewsChannel,
+  });
 
-    let files = await Promise.all(
-      msg.attachments.map(async (attachment) => {
-        try {
-          let res = await fetch(attachment.url);
-          let buffer = await res.buffer();
-          return { file: buffer, name: attachment.filename };
-        } catch (err) {
-          console.warning(`Couldn't fetch attachment from message ${msg.id}: ${attachment.url}`);
-          return null;
-        }
-      }),
-    );
-    files = files.filter((a) => a !== null);
+  let files = await Promise.all(
+    msg.attachments.map(async (attachment) => {
+      try {
+        let res = await fetch(attachment.url);
+        let buffer = await res.buffer();
+        return { file: buffer, name: attachment.filename };
+      } catch (err) {
+        console.warning(`Couldn't fetch attachment from message ${msg.id}: ${attachment.url}`);
+        return null;
+      }
+    }),
+  );
+  files = files.filter((a) => a !== null);
 
-    bot.createMessage(newsChannel, content, files);
-  },
-  {
-    description: ftl('news-cmd-description'),
-    fullDescription: ftl('news-cmd-long-description'),
-  },
-);
+  bot.createMessage(newsChannel, content, files);
+});
 
-bot.registerCommand(
-  'roll',
-  (msg) => {
-    try {
-      return rollDecider(stripContent(msg.content));
-    } catch (err) {
-      return err.toString();
-    }
-  },
-  {
-    description: ftl('roll-cmd-description'),
-    fullDescription: ftl('roll-cmd-long-description'),
-  },
-);
+bot.registerCommand('roll', (msg) => {
+  try {
+    return rollDecider(stripContent(msg.content));
+  } catch (err) {
+    return err.toString();
+  }
+});
 
-bot.registerCommand(
-  'acceptcoc',
-  (msg) => {
-    const adminChannel = process.env.ADMIN_CHANNEL_ID;
-    const cocRole = process.env.COC_ROLE_ID;
-    const userID = msg.member.id;
-    const guildID = msg.channel.guild.id;
-    const reason = 'member accepts the Code of Conduct';
+bot.registerCommand('acceptcoc', (msg) => {
+  const adminChannel = process.env.ADMIN_CHANNEL_ID;
+  const cocRole = process.env.COC_ROLE_ID;
+  const userID = msg.member.id;
+  const guildID = msg.channel.guild.id;
+  const reason = 'member accepts the Code of Conduct';
 
-    if (adminChannel) {
-      bot.createMessage(adminChannel, { content: ftl('accept-coc-admin-message', { userID }) });
-    }
-    if (cocRole) {
-      bot.addGuildMemberRole(guildID, userID, cocRole, reason);
-    }
-    bot.createMessage(msg.channel.id, { content: ftl('accept-coc-message-member') });
-  },
-  {
-    description: ftl('acceptcoc-cmd-description'),
-    fullDescription: ftl('acceptcoc-cmd-long-description'),
-  },
-);
+  if (adminChannel) {
+    bot.createMessage(adminChannel, { content: ftl('accept-coc-admin-message', { userID }) });
+  }
+  if (cocRole) {
+    bot.addGuildMemberRole(guildID, userID, cocRole, reason);
+  }
+  bot.createMessage(msg.channel.id, { content: ftl('accept-coc-message-member') });
+});
 
 bot.connect();
 
