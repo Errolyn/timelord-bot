5cbef7cbe54154700277f1a2940aa0b5daefa568
Errolyn <sam@fishy.ninja>
2020-08-25 09:58:47 -0700

    Move supporting logic for !roll command to own file (#34)
    * moved roll command into different file
    
    * moved roll command into different file
    
    * created diceRoller commandScript
    
    * fixed bug in dice roller
    
    * roll command is now contained
    
    * command accepts object
    
    Co-authored-by: Michael Cooper <mythmon@gmail.com>

---
+++ commands/diceRoller.js
@@ -0,0 +1,146 @@
+let ftl = require('../lib/ftl');
+
+module.exports.register = function (botArguments) {
+  const bot = botArguments.bot;
+  bot.registerCommand('roll', (msg) => {
+    let dice = stripContent(msg.content);
+    try {
+      return rollDecider(dice);
+    } catch (err) {
+      return err.toString();
+    }
+  });
+};
+
+function rollDecider(command) {
+  const commandArray = command.toLowerCase().split('');
+  const allowableCharacters = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+    'd',
+    'r',
+    ' ',
+    '+',
+    '-',
+  ];
+  const commandCharacters = ['d', 'r', '+', '-'];
+
+  commandArray.forEach((character) => {
+    if (!allowableCharacters.includes(character)) {
+      throw new Error(ftl('roll-error-invalid-chars'));
+    }
+  });
+
+  commandCharacters.forEach((character) => {
+    let tempIndex = commandArray.indexOf(character);
+    if (tempIndex > 0) {
+      if (commandArray.indexOf(character, tempIndex + 1) > 0) {
+        throw new Error(ftl('roll-error-multiple-parts'));
+      }
+    }
+    if (character === 'r' && tempIndex != -1 && tempIndex + 1 < commandArray.length) {
+      throw new Error(ftl('roll-error-r-not-at-end'));
+    }
+  });
+
+  const commandCleaned = command.toLowerCase().split(' ').join('').split('r').join(''); // removes spaces and Rs
+
+  const reroll = command.toLowerCase().includes('r');
+
+  let [amount, diceConfig] = commandCleaned.split('d');
+  amount = Number(amount);
+
+  if (amount <= 0 || isNaN(amount)) {
+    throw new Error(ftl('roll-error-atleast-one-die'));
+  }
+
+  if (amount > 100) {
+    throw new Error(ftl('roll-error-too-many-dice'));
+  }
+
+  let sides, modifier;
+  if (diceConfig === undefined) {
+    throw new Error(ftl('roll-error-no-dice-description'));
+  }
+
+  if (diceConfig.includes('+') && diceConfig.includes('-')) {
+    throw new Error(ftl('roll-error-too-many-operators'));
+  }
+
+  if (diceConfig.includes('+')) {
+    [sides, modifier] = diceConfig.split('+');
+    sides = Number(sides);
+    modifier = Number(modifier);
+    if (sides <= 1) {
+      throw new Error(ftl('roll-error-too-few-dice-sides'));
+    }
+    return ftl('roll-output', {
+      amount,
+      sides,
+      modifier: `+ ${modifier}`,
+      result: rollDice(amount, sides, reroll) + Number(modifier),
+    });
+  } else if (diceConfig.includes('-')) {
+    [sides, modifier] = diceConfig.split('-');
+    sides = Number(sides);
+    modifier = Number(modifier);
+    if (sides <= 1) {
+      throw new Error(ftl('roll-error-too-few-dice-sides'));
+    }
+    return ftl('roll-output', {
+      amount,
+      sides,
+      modifier: `- ${modifier}`,
+      result: rollDice(amount, sides, reroll) - Number(modifier),
+    });
+  } else {
+    sides = diceConfig;
+    sides = Number(sides);
+
+    if (sides <= 1) {
+      throw new Error(ftl('roll-error-too-few-dice-sides'));
+    }
+    return ftl('roll-output', {
+      amount,
+      sides,
+      modifier: '',
+      result: rollDice(amount, sides, reroll),
+    });
+  }
+}
+
+function getRandomNumber(max) {
+  let randomNumber = Math.floor(Math.random() * max) + 1;
+  return randomNumber;
+}
+
+function rollDice(amount, sides, reroll) {
+  let diceTotal = 0;
+
+  for (let roll = 0; roll < amount; roll++) {
+    let currentRoll;
+
+    do {
+      currentRoll = getRandomNumber(sides);
+    } while (reroll && currentRoll <= 1);
+
+    diceTotal += currentRoll;
+  }
+  return diceTotal;
+}
+
+function stripContent(messageContent) {
+  const stringParts = messageContent.split(' ');
+  stringParts.shift();
+
+  const userPost = stringParts.join(' ');
+  return userPost;
+}
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 let fetch = require('node-fetch');
-
 let BotWrapper = require('./lib/BotWrapper.js');
 let ftl = require('./lib/ftl.js');
-
 let pingCount = 0;
 
 // Heroku requires a port to be bound
@@ -80,17 +78,10 @@ bot.registerCommand('news', async (msg) => {
     }),
   );
   files = files.filter((a) => a !== null);
-
   bot.createMessage(newsChannel, content, files);
 });
 
-bot.registerCommand('roll', (msg) => {
-  try {
-    return rollDecider(stripContent(msg.content));
-  } catch (err) {
-    return err.toString();
-  }
-});
+require('./commands/diceRoller').register({ bot });
 
 bot.registerCommand('acceptcoc', (msg) => {
   const adminChannel = process.env.ADMIN_CHANNEL_ID;
@@ -118,129 +109,4 @@ function stripContent(messageContent) {
 
   const userPost = stringParts.join(' ');
   return userPost;
-}
-
-function rollDecider(command) {
-  const commandArray = command.toLowerCase().split('');
-  const allowableCharacters = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '0',
-    'd',
-    'r',
-    ' ',
-    '+',
-    '-',
-  ];
-  const commandCharacters = ['d', 'r', '+', '-'];
-
-  commandArray.forEach((character) => {
-    if (!allowableCharacters.includes(character)) {
-      throw new Error(ftl('roll-error-invalid-chars'));
-    }
-  });
-
-  commandCharacters.forEach((character) => {
-    let tempIndex = commandArray.indexOf(character);
-    if (tempIndex > 0) {
-      if (commandArray.indexOf(character, tempIndex + 1) > 0) {
-        throw new Error(ftl('roll-error-multiple-parts'));
-      }
-    }
-    if (character === 'r' && tempIndex != -1 && tempIndex + 1 < commandArray.length) {
-      throw new Error(ftl('roll-error-r-not-at-end'));
-    }
-  });
-
-  const commandCleaned = command.toLowerCase().split(' ').join('').split('r').join(''); // removes spaces and Rs
-
-  const reroll = command.toLowerCase().includes('r');
-
-  let [amount, diceConfig] = commandCleaned.split('d');
-  amount = Number(amount);
-
-  if (amount <= 0 || isNaN(amount)) {
-    throw new Error(ftl('roll-error-atleast-one-die'));
-  }
-
-  if (amount > 100) {
-    throw new Error(ftl('roll-error-too-many-dice'));
-  }
-
-  let sides, modifier;
-  if (diceConfig === undefined) {
-    throw new Error(ftl('roll-error-no-dice-description'));
-  }
-
-  if (diceConfig.includes('+') && diceConfig.includes('-')) {
-    throw new Error(ftl('roll-error-too-many-operators'));
-  }
-
-  if (diceConfig.includes('+')) {
-    [sides, modifier] = diceConfig.split('+');
-    sides = Number(sides);
-    modifier = Number(modifier);
-    if (sides <= 1) {
-      throw new Error(ftl('roll-error-too-few-dice-sides'));
-    }
-    return ftl('roll-output', {
-      amount,
-      sides,
-      modifier: `+ ${modifier}`,
-      result: rollDice(amount, sides, reroll) + Number(modifier),
-    });
-  } else if (diceConfig.includes('-')) {
-    [sides, modifier] = diceConfig.split('-');
-    sides = Number(sides);
-    modifier = Number(modifier);
-    if (sides <= 1) {
-      throw new Error(ftl('roll-error-too-few-dice-sides'));
-    }
-    return ftl('roll-output', {
-      amount,
-      sides,
-      modifier: `- ${modifier}`,
-      result: rollDice(amount, sides, reroll) - Number(modifier),
-    });
-  } else {
-    sides = diceConfig;
-    sides = Number(sides);
-
-    if (sides <= 1) {
-      throw new Error(ftl('roll-error-too-few-dice-sides'));
-    }
-    return ftl('roll-output', {
-      amount,
-      sides,
-      modifer: '',
-      result: rollDice(amount, sides, reroll),
-    });
-  }
-}
-
-function getRandomNumber(max) {
-  let randomNumber = Math.floor(Math.random() * max) + 1;
-  return randomNumber;
-}
-
-function rollDice(amount, sides, reroll) {
-  let diceTotal = 0;
-
-  for (let roll = 0; roll < amount; roll++) {
-    let currentRoll;
-
-    do {
-      currentRoll = getRandomNumber(sides);
-    } while (reroll && currentRoll <= 1);
-
-    diceTotal += currentRoll;
-  }
-  return diceTotal;
 }
