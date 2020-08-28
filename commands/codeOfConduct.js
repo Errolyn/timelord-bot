3dca27de2b84482f49a3dade1d45c25d56e91a81
Errolyn <sam@fishy.ninja>
2020-08-28 00:24:48 -0700

    code of conduct onjoin added

---
+++ commands/codeOfConduct.js
@@ -0,0 +1,38 @@
+let ftl = require('../lib/ftl');
+
+module.exports.register = (botArguments) => {
+  const bot = botArguments.bot;
+  bot.registerCommand('acceptcoc', (msg) => {
+    const adminChannel = process.env.ADMIN_CHANNEL_ID;
+    const cocRole = process.env.COC_ROLE_ID;
+    const userID = msg.member.id;
+    const guildID = msg.channel.guild.id;
+    const reason = 'member accepts the Code of Conduct';
+
+    if (adminChannel) {
+      bot.createMessage(adminChannel, { content: ftl('accept-coc-admin-message', { userID }) });
+    }
+    if (cocRole) {
+      bot.addGuildMemberRole(guildID, userID, cocRole, reason);
+    }
+    bot.createMessage(msg.channel.id, { content: ftl('acceptcoc-member-message') });
+  });
+};
+
+//This fires on the member joining event.
+module.exports.onJoin = (botArguments) => {
+  const bot = botArguments.bot;
+  const cocChannel = process.env.COC_CHANNEL_ID;
+  if (cocChannel) {
+    bot.on('guildMemberAdd', async (guild, member) => {
+      let userName = member.username; // in most places preferr to use nick name however nicks are not available to DMs
+      let serverName = guild.name; //guild in this case it the server
+      let channel = await bot.getDMChannel(member.id); //Finds the new member's DM Channel
+
+      bot.createMessage(
+        channel.id,
+        ftl('coc-welcome-prompt', { userName, serverName, cocChannel }),
+      );
+    });
+  }
+};
